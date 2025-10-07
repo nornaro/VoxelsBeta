0321cad1b954db87c778e82b9f971d20d521c6d5
ForlornU <101473036+ForlornU@users.noreply.github.com>
2025-10-07 16:11:22 +0200

    improved neighbor finding
    Planar neighbor search

--- scripts/Global/world.gd
+++ scripts/Global/world.gd
@@ -1,35 +1,38 @@
 extends Node
 
-#var map : Array[Voxel]
-var map_as_dict : Dictionary[Vector2i, Voxel] = {}
-var top_layer_voxels : Array[Voxel]
+var map_as_dict : Dictionary[Vector3i, Voxel] = {}
 var is_map_staggered = false
 var world_settings : GenerationSettings
 var noise_range : Vector2
 
-#var neighbor_positions = HEXAGONAL_NEIGHBOR_DIRECTIONS
 
 ## Construct a dictionary for our 2d top layer of voxels
 func set_map(voxels):
 	map_as_dict.clear()
 	for voxel : Voxel in voxels:
-		map_as_dict[Vector2i(voxel.grid_position_xz.x, voxel.grid_position_xz.y)] = voxel
+		map_as_dict[Vector3i(voxel.grid_position_xyz)] = voxel
+
 
 func clear_map():
 	map_as_dict.clear()
-	top_layer_voxels.clear()
 
-## Handy function for finding all neigbors of a tile
-#func get_tile_neighbors(tile : Tile) -> Array[Tile]:
-	#var neighbors : Array[Tile] = []
-	#if is_map_staggered:
-		#if tile.pos_data.grid_position.x % 2 == 0:
-			#neighbor_positions = NEIGHBOR_DIRECTIONS_EVEN
-		#else:
-			#neighbor_positions = NEIGHBOR_DIRECTIONS_ODD
-			#
-	#for direction in neighbor_positions:
-		#var neighbor_coords = Vector2(tile.pos_data.grid_position.x + int(direction.x), tile.pos_data.grid_position.y + int(direction.y)) 
-		#if neighbor_coords in map_as_dict:
-			#neighbors.append(map_as_dict[neighbor_coords])
-	#return neighbors
+
+## Handy function for finding all neigbors of a voxel
+func get_tile_neighbors_planar(voxel : Voxel) -> Array[Voxel]:
+	var neighbors : Array[Voxel] = []
+	var neighbor_positions = VoxelData.HEXAGONAL_NEIGHBOR_DIRECTIONS
+	if is_map_staggered:
+		if voxel.grid_position_xz.x % 2 == 0:
+			neighbor_positions = VoxelData.NEIGHBOR_DIRECTIONS_EVEN
+		else:
+			neighbor_positions = VoxelData.NEIGHBOR_DIRECTIONS_ODD
+			
+	for direction in neighbor_positions:
+		var neighbor_coords = Vector3i(
+			voxel.grid_position_xz.x + int(direction.x), #x + dir
+			 int(voxel.grid_position_xyz.y), # same y
+			 voxel.grid_position_xz.y + int(direction.y)) #z + dir
+			
+		if neighbor_coords in map_as_dict:
+			neighbors.append(map_as_dict[neighbor_coords])
+	return neighbors
